b1fe93fc62f5f5b26e710ddb802caa55d8726200
siswatelkom07 <140671201+siswatelkom07@users.noreply.github.com>
2024-01-09 20:44:55 +0700

    Add files via upload

---
+++ encapsiswa.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class encapsiswa {
+    private String name;
+    private String address;
+    private int age;
+    
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAge (int newAge){
+        age=newAge;
+    }
+    public void setName (String newName){
+        name=newName;
+    }
+    public void setAddress (String newAddress){
+        address=newAddress;
+    }
+    
+}
+
