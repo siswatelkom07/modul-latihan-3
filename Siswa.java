5e9b94d9d5b291116f5a91bfd5346f669deee8b4
siswatelkom07 <140671201+siswatelkom07@users.noreply.github.com>
2024-01-09 20:41:18 +0700

    Add files via upload

---
+++ Siswa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Siswa {
+    private String nama;
+    public int usia=17;
+    protected String namaTemanKomitmen="Alif";
+    public void setNama(String namaPanggilan){
+        this.nama=namaPanggilan;
+    }
+    public String getNama(){
+        return nama;
+    }
+}
---
+++ TestSiswa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TestSiswa {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    encapsiswa siswa = new encapsiswa();
+    siswa.setName("agus");
+    siswa.setAge(20);
+    siswa.setAddress("Malang");
+    
+        System.out.println("nama: "+siswa.getName()
+        + " alamat "+siswa.getAddress()+ " berumur "+siswa.getAge()
+        + " tahun");
+    }
+    
+}
+
